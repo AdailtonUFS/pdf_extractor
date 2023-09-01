be113cf0c6dc702f709fd81531d0c2828e40d259
Adailton Moura <contatoadailtonmoura@gmail.com>
2023-09-01 04:55:42 -0300

    change setup file

---
+++ setup_options.py
@@ -0,0 +1,53 @@
+from pdf_extractor.entities.Draw import Draw
+from pdf_extractor.entities.Ghostscript import Ghostscript
+from utils.open_pdf_viewer import open_pdf
+
+pdf_file_path = input("PDF FILE PATH (INPUT) \n")
+option = input("Escolha a opção: \n 1- Gerar PDF com as linhas \n 2- Gerar PDF com os retângulos \n 3- Gerar "
+               "postscript de um pdf\n 0- Sair \n")
+option = int(option)
+
+while option != 0:
+    path = input("Informe o caminho onde o arquivo será salvo:\n")
+    if option == 1 or option == 2:
+        coordinates = input(
+            "Deseja limitar a area do pdf?"
+            "\n Caso sim: digite as coordenadas separadas por \",\""
+            "\n xmin: \"Posição onde o arquivo começará a ser escrito no eixo x"
+            "\n xmin: \"Posição onde o arquivo terminará a ser escrito no eixo x"
+            "\n ymin: \"Posição onde o arquivo começará a ser escrito no eixo y"
+            "\n ymax: \"Posição onde o arquivo terminará a ser escrito no eixo y"
+            "\n Exemplo: 1230,8900,120,480 (desenha os dados das rochas e a forma da perfuração)"
+            "\n Exemplo: 1230,8893,295,359 (desenha os dados das rochas e a profundidade)"
+            "\n Exemplo: 1380,1385,280,310 (profundidade 50)"
+            "\n Exemplo: 1246,8877,0,2200  (teste distancia total)"
+            "\n Caso não: Apenas deixe em branco\n"
+        )
+
+        if len(coordinates.split(',')) == 4:
+            x_min, x_max, y_min, y_max = coordinates.split(',')
+            x_min = float(x_min)
+            x_max = float(x_max)
+            y_min = float(y_min)
+            y_max = float(y_max)
+
+            draw = Draw(pdf_file_path, x_min, x_max, y_min, y_max)
+        else:
+            draw = Draw(pdf_file_path)
+
+        if option == 1:
+            draw.line_pdf(path)
+            open_pdf(path)
+        if option == 2:
+            draw.complete_pdf(path)
+            open_pdf(path)
+
+    if option == 3:
+        print(pdf_file_path, path)
+        ghostscript = Ghostscript()
+        ghostscript.pdf_to_ps(pdf_file_path, path)
+
+    option = input(
+        "Escolha a opção: \n 1- Gerar PDF com as linhas \n 2- Gerar PDF com os retângulos \n 3- Gerar postscript de "
+        "um pdf\n 0- Sair \n")
+    option = int(option)
