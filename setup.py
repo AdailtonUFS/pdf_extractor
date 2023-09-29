8031a9f1add0f8f9d3ac5d5958bb8a42e0e91915
Adailton Moura <contatoadailtonmoura@gmail.com>
2023-09-29 19:41:15 -0300

    abstract the lithologies logic

--- setup.py
+++ setup.py
@@ -1,53 +1,16 @@
-import os
-
 from pdf_extractor.entities.Draw import Draw
 from pdf_extractor.entities.Image import Image
-from utils.uniquify_file import uniquify
+from utils.litologies import lithology_rect
+from utils.path import path_to_pdf_generated
 
-def define_pdf_path():
-    pdf_file_path = input("PDF FILE PATH (INPUT) \n")
+pdf_file_path = input("PDF FILE PATH (INPUT) \n")
+pdf_to_save_file = path_to_pdf_generated(pdf_file_path)
+image = Image(pdf_file_path)
+positions = image.get_lithologies_limit()
 
-    path_array = pdf_file_path.split('/')
+draw = Draw(pdf_file_path, y_coordinate_min=positions['profile_word_y'], y_coordinate_max=positions['lithology_word_y'])
+draw.complete_pdf(pdf_to_save_file)
 
-    filename = path_array[len(path_array) - 1]
-    folder_name = filename.replace('.pdf', '')
+lithology_rect(pdf_to_save_file)
 
-    path_folder = '/home/skywalker/projects/python/pdfquery/pdf_extractor/pdfs/generated/' + folder_name
-
-    if not os.path.exists(path_folder):
-        os.mkdir(path_folder)
-
-
-    path_save = uniquify(path_folder + "/" + filename)
-    return [pdf_file_path, path_save]
-
-
-pdf_file_path, path_save = define_pdf_path()
-
-position = {'x': 0, 'y': 0 , 'perf_x': 0, 'perf_y': 0}
-exists = False
-with open('litogias_location.txt', "a+") as file:
-    file.seek(0)
-    lines = file.readlines()
-
-    if len(lines) > 0:
-        for line in lines:
-            file_name, x, y, perfis_x, perfis_y = line.strip().split(';;')
-            if file_name == pdf_file_path:
-                position = {'x': float(x), 'y': float(y), 'perf_x': float(perfis_x), 'perf_y': float(perfis_y)}
-                exists = True
-                break
-
-    if not exists:
-        image = Image(pdf_file_path)
-        position_lit = image.search_word("litologias:")
-        perfis = image.search_word("perfis")
-        position = {'x': position_lit['x'], 'y': position_lit['y'], 'perf_x': perfis['x'], 'perf_y': perfis['y']}
-        file.write(pdf_file_path + ";;" + str(position['x']) + ";;" + str(position['y']) + ";;" + str(perfis['x']) + ";;" + str(perfis['y']) + "\n")
-
-
-file.close()
-print(position)
-draw = Draw(pdf_file_path, y_coordinate_min=position['perf_y'], y_coordinate_max=position['y'])
-draw.complete_pdf(path_save)
-
+# # /home/skywalker/projects/python/pdfquery/pdf_extractor/pdfs/3CP1853SE_PC.pdf
